02e3ff548118a322868c801083124ac379500c13
Delaksan-Sritharan <delaksan@gmail.com>
2025-04-13 22:59:25 +0530

    new

---
+++ src/main/java/Cal_rec.java
@@ -0,0 +1,13 @@
+public class Cal_rec {
+    public static void main(String [] args){
+        int length = 4;
+        int width = 2;
+        int area;
+
+        area = length * width;
+
+        System.out.println("The width of rectangle : " + width);
+        System.out.println("The length of rectange : " + length);
+        System.out.println("Area of the rectangle : "+ area);
+    }
+}
