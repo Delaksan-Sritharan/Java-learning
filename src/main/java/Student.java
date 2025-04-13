8556a9a57840440a9aa346eceb9ed22c8bf125d0
Delaksan-Sritharan <delaksan@gmail.com>
2025-04-13 21:55:13 +0530

    simplyfied code

---
+++ src/main/java/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public static void main(String[] args){
+        String studentName = "Delaksan Sritharan";
+        String studentID = "20240242";
+        int studentAge = 20;
+        float studentFee = 4000000;
+        char studentGrade = 'A';
+
+        System.out.println("Student Information");
+        System.out.println("Student name : " + studentName);
+        System.out.println("Student ID : " + studentID);
+        System.out.println("Student Age : " + studentAge);
+        System.out.println("Student Fee : "+ studentFee);
+        System.out.println("Student Grade : "+ studentGrade);
+
+    }
+}
