2001b958d06b8e71458d76249cdda1f7a7d37949
Delaksan-Sritharan <delaksan@gmail.com>
2025-04-14 16:27:07 +0530

    new

---
+++ src/main/java/wid_casting.java
@@ -0,0 +1,9 @@
+public class wid_casting {
+    public static void main(String []args){
+        int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
