ed2a15cc87f2750116b7277c46a698ec9bb0e636
Delaksan-Sritharan <delaksan@gmail.com>
2025-04-14 19:52:42 +0530

    new

---
+++ src/main/java/concat.java
@@ -0,0 +1,7 @@
+public class concat {
+    public static void main(String[] args) {
+        String fristName = "Delaksan";
+        String lastName = "Sritharan";
+        System.out.println(fristName.concat(lastName));
+    }
+}
