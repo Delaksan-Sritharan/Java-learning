dea6f863f4df3aa80b8646b1a854280f71821cd7
Delaksan-Sritharan <delaksan@gmail.com>
2025-04-14 16:30:46 +0530

    new

---
+++ src/main/java/narrow_casting.java
@@ -0,0 +1,9 @@
+public class narrow_casting {
+    public static void main(String[] args) {
+        int maxScore = 500;
+        int userScore = 423;
+
+        float percentage = (float) userScore/maxScore * 100.0f;
+        System.out.println("User's percentage is "+ percentage);
+    }
+}
