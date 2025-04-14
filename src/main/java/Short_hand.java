2a1894f0bbcb46ea5c3157e3d6691bb8bf1527cc
Delaksan-Sritharan <delaksan@gmail.com>
2025-04-14 19:56:25 +0530

    new

---
+++ src/main/java/Short_hand.java
@@ -0,0 +1,7 @@
+public class Short_hand {
+    public static void main(String[] args) {
+        int time = 20;
+        String result = (time<18) ? "good morning" : "good evening";
+        System.out.println(result);
+    }
+}
