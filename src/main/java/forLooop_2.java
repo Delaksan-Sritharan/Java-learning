ecc83a9d993c5da7c5a8a924f548e5bec26d294f
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-07 11:13:00 +0530

    Switch case

---
+++ src/main/java/forLooop_2.java
@@ -0,0 +1,8 @@
+public class forLooop_2 {
+    public static void main(String[]args){
+        int number = 2;
+        for (int i = 1;i <=10;i++){
+            System.out.println(number + " x "+ i + " = " + (number * i ));
+        }
+    }
+}
