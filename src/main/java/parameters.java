49611780b0dff1ceed4b825100faa1ca38e68f8e
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-08 20:34:43 +0530

    parameters example

---
+++ src/main/java/parameters.java
@@ -0,0 +1,11 @@
+public class parameters {
+    static void myMethod(String fname){
+        System.out.println(fname + " Refsnes");
+    }
+
+    public static void main(String []args){
+        myMethod("liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+}
