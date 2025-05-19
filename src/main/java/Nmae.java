062308ac62d0f990a768a057e5b265603d510076
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-19 19:03:45 +0530

    multiple attributes

---
+++ src/main/java/Nmae.java
@@ -0,0 +1,11 @@
+public class Nmae {
+    String fname = "Delaksan";
+    String lname = "Sritharan";
+    int age = 20;
+
+    public static void main(String []args){
+        Nmae myonj = new Nmae();
+        System.out.println("Name : " + myonj.fname+" "+myonj.lname);
+        System.out.println("Age : "+ myonj.age);
+    }
+}
