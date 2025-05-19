b3d22d1f7de077f37a2e47d8635582973f081e1d
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-19 18:55:34 +0530

    object

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    int x;
+
+    public static void main(String []args){
+        Main myobj = new Main();
+        myobj.x = 40;
+        System.out.println(myobj.x);
+
+    }
+}
---
+++ src/main/java/Second.java
@@ -0,0 +1,6 @@
+public class Second {
+    public static void main(String []args){
+        Main myobj = new Main();
+        System.out.println(myobj.x);
+    }
+}
