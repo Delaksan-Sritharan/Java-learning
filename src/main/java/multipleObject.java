3257da7c2139bf36c8376f2b3c6ca77b0b467dce
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-19 18:40:24 +0530

    object

---
+++ src/main/java/multipleObject.java
@@ -0,0 +1,12 @@
+public class multipleObject {
+    int x = 5;
+
+    public static void main(String[] args){
+        multipleObject myobj1 = new multipleObject();
+        multipleObject myobj2 = new multipleObject();
+        System.out.println(myobj1.x);
+        System.out.println(myobj2.x);
+    }
+}
+
+
