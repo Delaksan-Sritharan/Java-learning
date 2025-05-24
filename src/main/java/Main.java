02f3150458b8401f507d084d1e2255956120738b
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-24 10:53:51 +0530

    methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,18 @@
 public class Main {
-     int x = 5;
-
-    public static void main(String []args){
-        Main myobj1 = new Main();
-        Main myobj2 = new Main();
-        myobj2.x = 25;
-        System.out.println(myobj1.x);
-        System.out.println(myobj2.x);
-        myMethod();
-
+    static void myStaticMethod(){
+        System.out.println("Static methods can be called without creating objects");
     }
-    static void myMethod(){
-        System.out.println("Hello World!");
+
+    public void myPublicMethod(){
+        System.out.println("Public methods must be called by creaitng objects");
+    }
+
+    public static void main(String[]args){
+        myStaticMethod();
+//        myPublicMethod();
+
+        Main dela = new Main();
+        dela.myPublicMethod();
     }
 
 
