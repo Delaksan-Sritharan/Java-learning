06b12e74c19f7376e912dfefd83c06eafd9e6399
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-26 10:52:38 +0530

    get and set

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,7 @@
-abstract class Main{
-    public String fname = "Delaksan";
-    public int age = 20;
-    public abstract void study();
-}
-
-class Student extends Main {
-    public int graduationYear = 2028;
-
-    public void study() {
-        System.out.println("Studying all day long");
-
+public class Main{
+    public static void main(String[] args) {
+        Person myobj = new Person();
+        myobj.setName("Delaksan");
+        System.out.println(myobj.getName());
     }
 }
--- src/main/java/Second.java
+++ src/main/java/Second.java
@@ -1,10 +1,5 @@
 class Second{
     public static void main(String[] args) {
-        Student myobj = new Student();
 
-        System.out.println("Name: " + myobj.fname);
-        System.out.println("Age: " + myobj.age);
-        System.out.println("Graduation Year: " + myobj.graduationYear);
-        myobj.study();
 }
 }
