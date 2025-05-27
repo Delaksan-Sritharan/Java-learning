30ed149aa7291ff85ed610fcf41435b12e994c00
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-28 00:13:35 +0530

    Inner Classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,17 @@
 class OuterClass{
     int x = 10;
 
-    static class InnerClass{
-        int y = 5;
+    class InnerClass{
+        public int myInnerMethod(){
+            return x;
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        OuterClass.InnerClass myInner = new OuterClass.InnerClass();
-        System.out.println(myInner.y);
+        OuterClass myOuter = new OuterClass();
+        OuterClass.InnerClass myInner = myOuter.new InnerClass();
+        System.out.println(myInner.myInnerMethod());
     }
 }
