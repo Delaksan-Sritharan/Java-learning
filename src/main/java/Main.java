df80a4de220917d57d483933b8c0b1bdf4f103ab
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-04 11:00:29 +0530

    enums

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,29 @@
-class Main{
-    public static void main(String[]args){
-        Level obj = Level.MEDIUM;
-        System.out.println(obj);
+public class Main {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
 
-    }}
+    public static void main(String[] args) {
+        Level obj = Level.MEDIUM;
+        switch (obj) {
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+        }
+
+        // ---
+
+        // This loop was moved inside the main method
+        for (Level currentLevel : Level.values()) {
+            System.out.println(currentLevel);
+        }
+    }
+}
