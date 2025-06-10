d40702a63b1164d295cbac8cd97904e889625365
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-10 13:45:35 +0530

    ArrayList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,17 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
+import java.util.ArrayList;
 
 public class Main{
     public static void main(String[] args) {
-        LocalDateTime dateObj = LocalDateTime.now();
-        System.out.println("Before formatting: "+dateObj);
-        DateTimeFormatter formatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String fromattedDate = dateObj.format(formatObj);
-        System.out.println("After formatting : "+fromattedDate);
-        LocalDate obj = LocalDate.now();
-        LocalTime objTime = LocalTime.now();
-        LocalDateTime objDateTime = LocalDateTime.now();
-        System.out.println(obj);
-        System.out.println(objTime);
-        System.out.println(objDateTime);
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        cars.add("Audi");
+        System.out.println(cars);
+
+
+
     }
 
 }
