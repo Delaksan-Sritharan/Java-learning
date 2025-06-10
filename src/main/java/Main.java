2e70a3d5eb010f439c8cf9b8388a4b78dc64db79
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-10 13:54:31 +0530

    HashSet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class Main{
     public static void main(String[] args) {
-        HashMap<String,String> capitalCities = new HashMap<String,String>();
-        capitalCities.put("England","London");
-        capitalCities.put("Germany", "Berlin");
-        capitalCities.put("Norway", "Oslo");
-        capitalCities.put("USA", "Washington DC");
-        System.out.println(capitalCities);
+        HashSet<String> cars = new HashSet<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars);
     }
 
 }
