efad8964cae61218083540885c4c8ad903586e77
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-10 13:49:23 +0530

    HashMap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Main{
     public static void main(String[] args) {
-        LinkedList<String> cars = new LinkedList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        cars.add("Audi");
-        System.out.println(cars);
+        HashMap<String,String> capitalCities = new HashMap<String,String>();
+        capitalCities.put("England","London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
 
 
 
