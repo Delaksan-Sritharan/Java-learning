c74ea37191c54be1aa069288e97b88862c200404
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-14 19:22:23 +0530

    HashSet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,18 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> capitalCities = new HashMap<String, String>();
-        capitalCities.put("England", "London");
-        capitalCities.put("Germany", "Berlin");
-        capitalCities.put("Norway", "Oslo");
-        capitalCities.put("USA", "Washington DC");
-
-        for (String i : capitalCities.keySet()) {
-            System.out.println("key: "+ i+ " value : "+capitalCities.get(i));
+        HashSet< Integer> capitalCities = new HashSet<Integer>();
+        capitalCities.add(4);
+        capitalCities.add(7);
+        capitalCities.add(8);
+        for(int i = 0; i <=10; i++){
+            if(capitalCities.contains(i)){
+                System.out.println(i+" was found in the set");
+            }else {
+                System.out.println(i + " was not found in the set");
+            }
         }
     }
 }
