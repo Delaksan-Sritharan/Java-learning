3b09526e2521798d052168dd324e47df9888e342
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-14 19:32:35 +0530

    Iterators

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,23 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        HashSet< Integer> capitalCities = new HashSet<Integer>();
+        ArrayList< Integer> capitalCities = new ArrayList<Integer>();
         capitalCities.add(4);
         capitalCities.add(7);
         capitalCities.add(8);
-        for(int i = 0; i <=10; i++){
-            if(capitalCities.contains(i)){
-                System.out.println(i+" was found in the set");
-            }else {
-                System.out.println(i + " was not found in the set");
+        capitalCities.add(23);
+        Iterator<Integer> it = capitalCities.iterator();
+        while (it.hasNext()){
+            Integer i = it.next();
+            if(i < 10){
+                it.remove();
             }
         }
+        System.out.println(capitalCities);
+
     }
 }
