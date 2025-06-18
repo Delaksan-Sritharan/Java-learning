094fa12913433aca072f40336b8081faa19f7593
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-18 19:41:52 +0530

    even numbers using while loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,11 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Even numbers");
-        for(int i = 0; i <=100 ; i++){
-            if(i % 2 == 0){
+        int i = 0;
+        while(i<=100){
+            if (i%2 ==0){
                 System.out.println(i);
-            }
+            }i++;
         }
     }
 }
