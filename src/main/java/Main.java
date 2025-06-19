425467b00b0bbb03b0b0b9f995b560e4c561875d
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-19 18:50:48 +0530

    add num

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> luckyNumberList = new ArrayList<Integer>();
-        Random rand = new Random();
-        while (luckyNumberList.size() < 6){
-            int randomNumber = rand.nextInt(49)+1;
-            if(!luckyNumberList.contains(randomNumber)){
-                luckyNumberList.add(randomNumber);
-            }
-        }
+        int x,y,sum;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        x = input.nextInt();
+        System.out.println("Type another number: ");
+        y = input.nextInt();
+
+        sum = x + y;
+        System.out.println("Sum is: "+sum);
+
     }
 }
