445936b6e87595ccbcf5c520949e6213028d01d1
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-20 22:37:40 +0530

    reversed word

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-       String str = "Hello";
-       String Rstr = "";
+       int [] myArray = {7,12,9,4,11};
+       int minVal = myArray[0];
 
-       for (int i = 0; i < str.length();i++){
-           Rstr = str.charAt(i) + Rstr;
-       }
-        System.out.println("Reversed String : "+ Rstr);
+       for (int i: myArray){
+           if (i < minVal){
+               minVal = i;
+           }
+       } System.out.println("Lowest Value: "+minVal);
+       //nice
     }
 }
